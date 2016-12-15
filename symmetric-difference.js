d7a91cc3934175ff005a224fc62a3049fb458614
Dominic <haakym@gmail.com>
2016-12-15 14:11:55 +0000

    Update symmetric-difference.js

--- symmetric-difference.js
+++ symmetric-difference.js
@@ -1,34 +1,34 @@
 // https://www.freecodecamp.com/challenges/symmetric-difference
 
 function sym(args) {
-	const sets = [].slice.call(arguments);
+  const sets = [].slice.call(arguments);
 
-	const result = sets
-		.map(removeDuplicates)
-		.reduce(symmetricDifference, []);
+  const result = sets
+    .map(removeDuplicates)
+    .reduce(symmetricDifference, []);
 
-	return result;
+  return result;
 }
 
 function removeDuplicates(set) {
-	return set.filter(function(item, pos, self) {
-		return self.indexOf(item) == pos;
-	});
+  return set.filter(function(item, pos, self) {
+    return self.indexOf(item) == pos;
+  });
 }
 
 function symmetricDifference(currentSet, newSet) {
-	if (currentSet.length === 0) return currentSet.concat(newSet);
+  if (currentSet.length === 0) return currentSet.concat(newSet);
 
-	newSet.forEach(num => {
-		const indexOfNum = currentSet.indexOf(num);
+  newSet.forEach(num => {
+    const indexOfNum = currentSet.indexOf(num);
 
-		if (indexOfNum > -1) {
-			currentSet.splice(indexOfNum, 1);			
-		} else {
-			currentSet.push(num);
-		}
+    if (indexOfNum > -1) {
+      currentSet.splice(indexOfNum, 1);			
+    } else {
+      currentSet.push(num);
+    }
 
-	});
+  });
 
-	return currentSet;
+  return currentSet;
 }
